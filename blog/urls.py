402b21a6da04a1892e7b2222a2d408b1c6e275d9
Florence <florenceshiru765@gmail.com>
2021-04-27 21:40:01 +0300

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, post_detail
+from . import views
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('/', views.post_detail, name='post_detail'),
+]
